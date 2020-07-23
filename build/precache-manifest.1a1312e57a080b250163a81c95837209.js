c3a3d08a3dfe9e3b7775c074de486bf2b3ff4486
hxnain619 <hxan619@gmail.com>
2020-07-23 22:44:06 +0500

    updates

---
+++ build/precache-manifest.1a1312e57a080b250163a81c95837209.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b717ef59962f37d09013494329c87b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d3cae7869a90ac190c1",
+    "url": "/static/css/2.aa764512.chunk.css"
+  },
+  {
+    "revision": "9a77368b9c9f342e4c58",
+    "url": "/static/css/main.962870ee.chunk.css"
+  },
+  {
+    "revision": "5d3cae7869a90ac190c1",
+    "url": "/static/js/2.c00637b4.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.c00637b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a77368b9c9f342e4c58",
+    "url": "/static/js/main.9c335055.chunk.js"
+  },
+  {
+    "revision": "2fef7df4df8b2aa5d8c0",
+    "url": "/static/js/runtime-main.6dd6687b.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "e82ae5b31935b253e8f5fd7b3e805286",
+    "url": "/static/media/hongkong.e82ae5b3.png"
+  },
+  {
+    "revision": "1c1b7c0946ce72f343efaff70e1bb676",
+    "url": "/static/media/imageA.1c1b7c09.png"
+  },
+  {
+    "revision": "4232bc3a03f21a0a7e5d3861fd5bd00e",
+    "url": "/static/media/imageB.4232bc3a.png"
+  },
+  {
+    "revision": "39b68e5630ab6dedca6aa4ad55362135",
+    "url": "/static/media/imageC.39b68e56.png"
+  },
+  {
+    "revision": "756b5b0f6ef8c3a096c0c26de86770e0",
+    "url": "/static/media/imageSmall_1.756b5b0f.png"
+  },
+  {
+    "revision": "42084e3adc9e1735d1aa3f9c4a9cbb93",
+    "url": "/static/media/imageSmall_2.42084e3a.png"
+  },
+  {
+    "revision": "2adc0b222a668fa32a9ca29c2cb8feed",
+    "url": "/static/media/imageSmall_3.2adc0b22.png"
+  },
+  {
+    "revision": "5f0e43948809051677a79a471d19c02c",
+    "url": "/static/media/imageSmall_4.5f0e4394.png"
+  },
+  {
+    "revision": "4d1d0d6f1f5b9e0ab10ac8ba043e3958",
+    "url": "/static/media/imageSmall_5.4d1d0d6f.png"
+  },
+  {
+    "revision": "5efab3e717f5c6a841ada6a2e429e74c",
+    "url": "/static/media/imageSmall_6.5efab3e7.png"
+  },
+  {
+    "revision": "e1ddf0c66f2ae32f018779fc442abb44",
+    "url": "/static/media/japan.e1ddf0c6.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a8c652fca908048d3bb5a14c05881180",
+    "url": "/static/media/mapGraphic.a8c652fc.png"
+  },
+  {
+    "revision": "89398c9a4ce9d52b6c9e900d54c55a1e",
+    "url": "/static/media/singapore.89398c9a.png"
+  }
+]);
